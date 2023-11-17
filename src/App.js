b728b429c15e13ac5cb0dd9ef828c2ab08963798
knt0214 <tantanboo0214@gmail.com>
2023-11-17 20:29:06 +0900

    残りのタスク機能実装

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: "Todo1", completed: false}]);
+  const [todos, setTodos] = useState([]);
 
   const todoNameRef = useRef();
 
@@ -29,7 +29,7 @@ function App() {
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
