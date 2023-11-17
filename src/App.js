8c3ea0bd089e1ccc3d8ab46cf6cc467577b04a39
knt0214 <tantanboo0214@gmail.com>
2023-11-17 20:33:31 +0900

    完了したタスクの削除機能実装

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
 
   const handleAddTodo = () => {
     //タスクを追加
-    const name = todoNameRef.current.value
+    const name = todoNameRef.current.value;
+    if (name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, {id: uuidv4(), name: name, completed: false}];
     });
@@ -23,12 +24,17 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
+
   return (
     <div>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
